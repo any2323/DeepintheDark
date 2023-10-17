aad2e9a8e2bd78ea3601769931e78caab439e424
kmk5844 <tjdwnd5844@naver.com>
2023-10-17 13:28:38 +0900

    kmk-UI Repeat

--- 유니티/Project_1/Assets/Script/Common/PlayerPrefsDelete.cs
+++ 유니티/Project_1/Assets/Script/Common/PlayerPrefsDelete.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerPrefsDelete : MonoBehaviour
 {
     public void onPlayerPrefsDelete()
     {
         PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
